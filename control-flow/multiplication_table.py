a875b133df17c51045716275fefb4b032c545132
bolajeee <ibrahimomobolaji1999@gmail.com>
2025-05-31 10:28:35 +0100

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 # This program generates a multiplication table for a given number.
-number = input(" Enter a number to see its multiplication table: ")
+number = input("Enter a number to see its multiplication table: ")
 
 for i in range(1, 11):
     result = i * int(number)
