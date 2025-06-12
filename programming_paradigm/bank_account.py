2e26e999be0f0513cf7c7ff26f0122e3b3909f12
bolajeee <ibrahimomobolaji1999@gmail.com>
2025-06-12 23:36:02 +0100

    bank_account created

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount():  # A simple bank account class to demonstrate object-oriented programming principles
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance # Initialize the account with a account_balance of 0
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+        else:
+            raise ValueError("Deposit amount must be positive") # Ensure that the deposit amount is positive
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            raise ValueError("Withdrawal amount must be positive and less than or equal to the account_balance") # Ensure that the withdrawal amount is positive and does not exceed the account_balance
+            return False
+
+    def display_balance(self, currency='USD'):
+        return f"Account Balance: {self.account_balance} {currency}"
+          # Display the account balance in the specified currency, defaulting to USD
+
